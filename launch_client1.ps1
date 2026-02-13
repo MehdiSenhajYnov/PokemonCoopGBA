fbdb0959a3afdaf7f9e0666f3cfbda1eb3b5b76f
MehdiSenhajYnov <93327385+MehdiSenhajYnov@users.noreply.github.com>
2026-02-14 00:08:28 +0100

    chore: drop root helper and notes files from repo

--- launch_client1.ps1
+++
@@ -1,2 +0,0 @@
-# Launch Client 1 (Master) - Save State 1 (Chimchar Lv12) - AUTO-DUEL REQUESTER
-& "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\mgba\mGBA.exe" -t "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\rom\Pokemon RunBun.ss1" --script "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\client\auto_duel_requester.lua" "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\rom\Pokemon RunBun.gba"
--- launch_server.ps1
+++
@@ -1,2 +0,0 @@
-# Launch the TCP relay server (output to server_log.txt)
-node "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\server\server.js" 2>&1 | Tee-Object -FilePath "C:\Users\mehdi\Desktop\Dev\PokemonCoopGBA\server_log.txt"
